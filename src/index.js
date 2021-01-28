fa7ade8617276839a49e8912161e2730b4e76d94
ricardocastanho <ricardinhorl@hotmail.com>
2021-01-27 22:34:22 -0300

    :heavy_plus_sign: add eslint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server')
- 
+
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `
- 
+
 const resolvers = {
   Query: {
     hello: () => 'Hello world!'
   }
 }
- 
-const server = new ApolloServer({ typeDefs, resolvers });
+
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
