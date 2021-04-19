45fa471015eab42568dcd5499376f9f64865f644
ricardocastanho <ricardinhorl@hotmail.com>
2021-04-19 13:28:35 -0300

    🚨 changing linter configurations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import 'module-alias/register'
 import app from '@/main/config/app'
 
-app.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+app.listen()
+  .then(({ url }): void => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
+  .catch(error => {
+    console.log(error)
+  })
