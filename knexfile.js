75a0b3631f43feaf88b58b26a76e374712ee7915
ricardocastanho <ricardinhorl@hotmail.com>
2021-05-24 08:23:41 -0300

    🔧 update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,21 @@
 const path = require('path')
-const { config } = require('./src/infra')
 
 const absolute = dest => path.join(__dirname, dest)
 
+const dotenv = require('dotenv')
+dotenv.config({
+  path: path.join(__dirname, './.env')
+})
+
 module.exports = {
-  ...config,
+  client: 'pg',
+  connection: Object.freeze({
+    port: process.env.DB_PORT || '5000',
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'local'
+  }),
   pool: {
     min: 2,
     max: 10
