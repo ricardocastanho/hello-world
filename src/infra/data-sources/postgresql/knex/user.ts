ea8c274eee93670ac8b7cd7036ee96fa6f7db0a0
ricardocastanho <ricardinhorl@hotmail.com>
2021-05-25 13:02:44 -0300

    👷 this should break it again

--- src/infra/data-sources/postgresql/knex/user.ts
+++ src/infra/data-sources/postgresql/knex/user.ts
@@ -1,4 +1,4 @@
-import knex from '@/main/config/knex'
+// import knex from '@/main/config/knex'
 import { PostgresqlUser } from '@/infra/contracts'
 
 export const loadUsers = async (): Promise<PostgresqlUser[]> => null
