51463dc039e2802a7d64d15a984dced1c378d1dc
ricardocastanho <ricardinhorl@hotmail.com>
2021-05-29 20:12:22 -0300

    ✅ add typeDefs tests

---
+++ tests/main/graphql/typeDefs/schemas.spec.ts
@@ -0,0 +1,28 @@
+import { gql } from 'apollo-server'
+import typeDefs from '@/main/graphql/type-defs'
+
+const base = gql`
+  type Query {
+    _: String
+  }
+`
+
+const user = gql`
+  extend type Query {
+    loadUsers: [User!]
+  }
+
+  type User {
+    id: String
+    firstName: String
+    lastName: String
+    email: String
+    createdAt: String
+    updatedAt: String
+    deletedAt: String
+  }
+`
+
+it('should return loadUsers schema', () => {
+  expect(typeDefs).toEqual([base, user])
+})
