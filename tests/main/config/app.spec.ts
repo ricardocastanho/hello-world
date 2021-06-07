c225f618b33572ab66cb87eba9583af31025d80a
ricardocastanho <ricardinhorl@hotmail.com>
2021-06-07 12:44:57 -0300

    🔥 remove unecessary test

--- tests/main/config/app.spec.ts
+++ tests/main/config/app.spec.ts
@@ -1,19 +1,9 @@
 import { ApolloServer } from 'apollo-server'
-import { GraphQLScalarType } from 'graphql'
 
 import app from '@/main/config/app'
 
 it('should build ApolloServer instance', () => {
   expect(app).toBeInstanceOf(ApolloServer)
-  expect(app).toHaveProperty('config.resolvers', [
-    {
-      Query: {
-        loadUsers: expect.any(Function)
-      }
-    },
-    {
-      Upload: expect.any(GraphQLScalarType)
-    }
-  ])
+  expect(app).toHaveProperty('config.resolvers')
   expect(app).toHaveProperty('config.typeDefs')
 })
